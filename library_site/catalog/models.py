da91c5fb37d4d49a4dfeaf13467b3943cfd23dae
Lorrimer Mann <lorrimer.mann@gmail.com>
2021-01-12 12:24:11 -0500

    Complete admin setup Completed model comments, registered models in admin and added inlines, changed book str method now returns name correctly

--- library_site/catalog/models.py
+++ library_site/catalog/models.py
@@ -1,46 +1,56 @@
 from django.db import models
-from django.urls import reverse  # generates URLs by reversing the URL patterns
-import uuid  # Required for unique book instances
+from django.urls import reverse
+import uuid
 
 
 class Genre(models.Model):
-    """Model representing a book genre."""
+    """These models represent the structure of all our stored data. Models are required
+        for defining what things look like (i.e. books, authors, genres, etc)"""
     name = models.CharField(max_length=200,
                             help_text='Enter a book genre (e.g Fantasy)')
 
     def __str__(self):
-        """String for representing the Model object."""
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a book language (e.g. Spanish)')
+
+    def __str__(self):
+        """returns a string for every one of these objects that is displayed"""
         return self.name
 
 
 class Book(models.Model):
-    """Model representing a book (but not a specific copy of a book)."""
+    """Model for the books themselves (not the individual copies of the books)"""
     title = models.CharField(max_length=200)
-    # Foreign Key used because book can only have one author
-    # authors can have multiple books
-    # Author as a string rather than object because it hasn't been declared yet
+    # FK to string rather than object because it has not been declared yet
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000,
                                help_text='A brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, unique=True,
                              help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
 
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.title
+    def display_genre(self):
+        """ManyToMany relationships are hard to access for many objects
+            so this method returns a string with the top 3 Genre entries instead of the Genre objects"""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
+    def __str__(self):
+        return self.title
+
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
-    book = models.ForeignKey(Book, on_delete=models.RESTRICT)
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
 
@@ -63,7 +73,6 @@ class BookInstance(models.Model):
         ordering = ['due_back']
 
     def __str__(self):
-        """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
 
 
@@ -78,9 +87,7 @@ class Author(models.Model):
         ordering = ['last_name', 'first_name']
 
     def get_absolute_url(self):
-        """Returns the url to access a particular author instance."""
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
-        """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
